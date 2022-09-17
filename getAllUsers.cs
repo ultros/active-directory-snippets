5562a20e856a9840049e70692b2c338d394f8c6c
Jesse Shelley <jesse@cybertutorials.org>
2022-09-17 14:44:12 -0600

    Create getAllUsers.cs

---
+++ getAllUsers.cs
@@ -0,0 +1,35 @@
+static void main()
+{
+    getAllUsers();
+}
+
+static void getAllUsers()
+{
+    DirectoryEntry myLdapCon = createDirectoryEntry();
+
+    DirectorySearcher searcher = new DirectorySearcher(myLdapCon);
+    searcher.Filter = "(&(objectCategory=User))";
+
+    Console.WriteLine("--- Searcher Started ---");
+    SearchResultCollection result = searcher.FindAll();
+
+    foreach (SearchResult user in result)
+    {
+        Console.WriteLine(user.Properties["samaccountname"].OfType<object>().FirstOrDefault());
+    }
+
+    Console.WriteLine("--- Completed ---");
+    Console.ReadLine();
+}
+
+static DirectoryEntry createDirectoryEntry()
+{
+    //build connection string including active directory username (and password)
+    DirectoryEntry ldapConnection = new DirectoryEntry();
+    ldapConnection.Username = "john";
+    ldapConnection.Password = "P@ssw0rd";
+    ldapConnection.Path = "LDAP://dc.test.local/CN=Users,DC=test,DC=local";
+    ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+
+    return ldapConnection;
+}
